9d0a653dc724ff911e799c31dc275f63c74245f3
Austin Cummings <aac729@live.com>
2020-10-07 18:44:19 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,7 +1,13 @@
-class SecretsController
-  
-  def before_action
-    
-  end 
-  
+class SecretsController < ApplicationController
+  before_action :require_login
+
+  def show
+  end
+
+  private
+  def require_login
+    unless session.include? :name
+      redirect_to :login
+    end
+  end
 end
